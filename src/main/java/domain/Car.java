ced2961a2033efb70afc22347ca88ab5b8e38486
Daeil Jung <wjdeodlf0123@gmail.com>
2019-12-10 18:50:41 +0900

    implement getter&setter
    1. implement getter&setter
    2. implementing playing in main...

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -8,6 +8,14 @@ public class Car {
 		this.name = name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
 	// 움직이거나 멈추는 메소드
 	// 전진하는 조건은 0에서 9사이에서 random 값을 구한 후 random 값이 4이상일 경우 전진하고
 	// 3 이하의값이면멈춘다.
