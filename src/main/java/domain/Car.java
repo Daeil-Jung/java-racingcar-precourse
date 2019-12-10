9745c8c0e64f6e4f0acdcd9f2077741a23d2da2a
Daeil Jung <wjdeodlf0123@gmail.com>
2019-12-10 19:00:43 +0900

    car::play method
    1. implement play method in car object
    2. implementing playing in main....

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -17,8 +17,15 @@ public class Car {
 	}
 
 	// 움직이거나 멈추는 메소드
-	// 전진하는 조건은 0에서 9사이에서 random 값을 구한 후 random 값이 4이상일 경우 전진하고
-	// 3 이하의값이면멈춘다.
+	public void game() {
+		// 전진하는 조건은 0에서 9사이에서 random 값을 구한 후 random 값이 4이상일 경우 전진하고
+		// 3 이하의값이면멈춘다.
+		double randomValue = Math.random();
+		int conditionValue = (int)(randomValue * 10);
+		if (conditionValue >= 4) {
+			position++;
+		}
+	}
 
 	// 이름의 적합을 판단하는 메소드
 
