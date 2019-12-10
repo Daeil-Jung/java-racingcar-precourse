f6963ef29ce10f575aeb828dc4e4258011655b6d
Daeil Jung <wjdeodlf0123@gmail.com>
2019-12-10 17:30:47 +0900

    accept code rules
    1. 자바 코드 룰 적용
    2. 메인 함수 프레임 생성

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,17 +1,17 @@
 package domain;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 움직이거나 멈추는 메소드
-    // 전진하는 조건은 0에서 9사이에서 random 값을 구한 후 random 값이 4이상일 경우 전진하고
-    // 3 이하의값이면멈춘다.
+	// 움직이거나 멈추는 메소드
+	// 전진하는 조건은 0에서 9사이에서 random 값을 구한 후 random 값이 4이상일 경우 전진하고
+	// 3 이하의값이면멈춘다.
 
-    // 이름의 적합을 판단하는 메소드
+	// 이름의 적합을 판단하는 메소드
 
 }
